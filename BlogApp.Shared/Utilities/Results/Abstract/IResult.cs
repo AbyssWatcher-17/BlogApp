98199479da5944fd4706f89fd9545b1ce883442a
yavuz <softwarelanguage@hotmail.com>
2024-02-12 19:32:03 +0300

    Added Result Layer to Project

---
+++ BlogApp.Shared/Utilities/Results/Abstract/IResult.cs
@@ -0,0 +1,19 @@
+﻿using BlogApp.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Shared.Utilities.Results.Abstract
+{
+    public interface IResult
+    {
+        public ResultStatus ResultStatus { get;  }
+
+        public string Message { get;  }
+
+        public Exception Exception { get;  }
+
+    }
+}
---
+++ BlogApp.Shared/Utilities/Results/Concrete/Result.cs
@@ -0,0 +1,38 @@
+﻿using BlogApp.Shared.Utilities.Results.ComplexTypes;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlogApp.Shared.Utilities.Results.Concrete
+{
+    public class Result
+    {
+
+        public Result(ResultStatus resultStatus)
+        {
+            ResultStatus =  resultStatus;
+        }
+
+        public Result(ResultStatus resultStatus,string message)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+        }
+
+        public Result(ResultStatus resultStatus, string message,Exception exception)
+        {
+            ResultStatus = resultStatus;
+            Message = message;
+            Exception = exception;
+        }
+
+        public ResultStatus ResultStatus { get; }
+
+        public string Message { get; }
+
+        public Exception Exception { get; }
+        // new Result(ResultStatus.Error,exception.message,exception)
+    }
+}
